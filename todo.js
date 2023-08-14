55963b8983e1f8b3e09b5a6c56ec89dbbc7dc4c6
Ashwin521 <ashwingiri521@gmail.com>
2023-08-14 18:13:14 +0530

    updated

---
+++ todo.js
@@ -0,0 +1,8 @@
+const item=document.querySelector("#item")
+const todo=document.querySelector("#to-do box")
+item.addEventListener(
+    "keyup",
+    function(event){
+        console.log(event)
+    }
+)
